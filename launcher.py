39b202399b4e2791c3017d917d3a72f6f9db1485
sunkenxn <97480325+sunkenAZ@users.noreply.github.com>
2025-03-07 21:50:58 +0200

    v4

--- launcher.py
+++ launcher.py
@@ -1,82 +1,112 @@
-import os as OS;
-import json as JSON;
 import subprocess as Subprocess;
+import json as JSON;
+import math as Math;
+import os as OS;
 
-OS.system('mode con: cols=64 lines=17');
-OS.system("TITLE lethal's totally tubular unn2 launcher");
+OS.system("TITLE lsl for unn2 v4");
 
-ef_to_launch: str = ".exe";
+ef_to_launch: str = 'exe';
 
 cmds: dict = {
-    "INFO  (version id)": "Prints info about the selected version.",
-    "START (version id)": "Opens the selected version.",
-    "QUIT": "Exits this program."
+    'INFO  (version id)': 'Prints info about the selected version.',
+    'START (version id)': 'Opens the selected version.',
+    'SETTINGS': 'Modify launcher settings.',
+    'QUIT': 'Exits this program.'
 };
 
+s_cmds: dict = {
+    'BACK': ['Go back to main launcher.', '--'],
+    'COLOR': ['Modify color. Identical to CMD COLOR.', '07']
+};
+
+in_settings: bool = False;
+
 while True:
-    OS.system("CLS");
-    print("lethal's Totally Tubular unn2 version launcher 3.0.0");
-    print("~" * 64);
-    
-    for cmd in cmds:
-        print(f'{cmd:18s} : {cmds[cmd]}');
-
-    print("~" * 64);
-    
     cwd: str = OS.getcwd();
-    mods: list = OS.listdir(f'{cwd}\\builds\\');
+    games: list = OS.listdir(f'{cwd}\\games\\');
     
-    m_id: int = 0;
-    for mod_f in mods:
-        mod_f_length: int = len(mod_f);
+    OS.system(f'mode con: cols=64 lines={6 + len(games) + len(cmds)}');
+    OS.system('CLS');
+    print(f'lethal\'s simple unn2 launcher {"<2025/03/07 - version 4>":>34}');
+    print('~' * 64);
+    
+    if in_settings:
+        for cmd in s_cmds:
+            print(f'{cmd:18s} : {s_cmds[cmd][1]} : {s_cmds[cmd][0]}');
+    else:
+        for cmd in cmds:
+            print(f'{cmd:18s} : {cmds[cmd]}');
         
-        if mod_f[mod_f_length-4:mod_f_length] == ef_to_launch:
-            print(f'[{m_id}] {mod_f[0:mod_f_length-4]}');
-            m_id += 1;
+        print('~' * 64);
+    
+        g_id: int = 0;
+        for game_f in games:
+            game_name_length: int = len(game_f);
+        
+            if game_f[game_name_length - len(ef_to_launch) - 1:game_name_length] == f'.{ef_to_launch}':
+                print(f'[{g_id}] > {game_f[0:game_name_length - len(ef_to_launch) - 1]}');
+                g_id += 1;
         
     print("~" * 64);
 
-    m_input: str = input('ACT ').split(' ');
+    m_input: str = input('DO? ').split(' ');
     OS.system('CLS');
 
-    if(m_input[0].upper() == "INFO"):
-        if(int(m_input[1])*2+1 > len(mods)):
-            print(f'Provided m_id too high ({int(m_input[1])*2+1} < {len(mods)}). Lowering to last valid m_id.\n');
-            m_input[1] = str((len(mods) - 1) / 2);
+    # :: INFO ::
+    if(m_input[0].upper() == 'INFO'):
+        if int(m_input[1]) > len(games) or int(m_input[1]) < 0:
+            print(f'Provided g_id out of range (-1 < {int(m_input[1])} < {len(games)}). Clamping to last valid m_id. ({min(len(games)-1, max(int(m_input[1]), 0))})\n');
+            m_input[1] = str(min(len(games)-1, max(int(m_input[1]), 0)));
 
-        with open(f'{cwd}\\builds\\{mods[int(m_input[1])*2+1]}', 'r') as mod_info:
-            mod_info_data: dict = JSON.loads(mod_info.read());
-            print(f'{mod_info_data['name']} by {mod_info_data['developer']}\n{mod_info_data['description']}\n');
-            print(f'[save file: {mod_info_data['save_location']}->{mod_info_data['save_identifier']}]\n');
+        with open(f'{cwd}\\infos\\{games[int(m_input[1])][0:len(games[int(m_input[1])])-len(ef_to_launch)-1]}.json', 'r') as ver_info:
+            ver_info_data: dict = JSON.loads(ver_info.read());
+            print(f'{ver_info_data['name']} by {ver_info_data['developer']}\n{ver_info_data['description']}\n');
+            print(f'[save file: {ver_info_data['save_location']} -> (cloned to){ver_info_data['save_identifier']}]\n');
             OS.system('PAUSE');
     
-    if(m_input[0].upper() == "START"):
-        with open(f'{cwd}\\builds\\{mods[int(m_input[1])*2+1]}', 'r') as mod_info:
-            mod_info_data: dict = JSON.loads(mod_info.read());
-            print(f'Opening {mods[int(m_input[1])*2]}\nSpoofing save with {mod_info_data['save_identifier']}');
+    # :: START ::
+    if(m_input[0].upper() == 'START'):
+        with open(f'{cwd}\\infos\\{games[int(m_input[1])][0:len(games[int(m_input[1])])-len(ef_to_launch)-1]}.json', 'r') as game_info:
+            game_info_data: dict = JSON.loads(game_info.read());
+            print(f'Opening {games[int(m_input[1])]}\n& spoofing save -> {game_info_data['save_identifier']}');
             
-            save_path: str = mod_info_data['save_location'];
+            save_path: str = game_info_data['save_location'];
             if save_path.split('\\')[0] == "%%mmf":
                 save_path = f'{OS.getenv('APPDATA')}\\MMFApplications\\{save_path.split('\\')[1]}';
             
-            with open(f'{cwd}\\save_clones\\{mod_info_data['save_identifier']}', 'r') as sv_f:
+            with open(f'{cwd}\\save_clones\\{game_info_data['save_identifier']}', 'r') as sv_f:
                 with open(save_path, 'w') as w_f:
                     w_f.write(sv_f.read());
-            
-        Subprocess.call([f'{cwd}\\builds\\{mods[int(m_input[1])*2]}']);
         
-        with open(f'{cwd}\\builds\\{mods[int(m_input[1])*2+1]}', 'r') as mod_info:
-            mod_info_data: dict = JSON.loads(mod_info.read());
-            print(f'Saving spoofed save from {mod_info_data['save_location']} as {mod_info_data['save_identifier']}');
+        Subprocess.call([f'{cwd}\\games\\{games[int(m_input[1])]}']);
+        
+        with open(f'{cwd}\\infos\\{games[int(m_input[1])][0:len(games[int(m_input[1])])-len(ef_to_launch)-1]}.json', 'r') as game_info:
+            game_info_data: dict = JSON.loads(game_info.read());
+            print(f'Saving spoofed save from {game_info_data['save_location']} as {game_info_data['save_identifier']}');
             
-            save_path: str = mod_info_data['save_location'];
+            save_path: str = game_info_data['save_location'];
             if save_path.split('\\')[0] == "%%mmf":
                 save_path = f'{OS.getenv('APPDATA')}\\MMFApplications\\{save_path.split('\\')[1]}';
                 
             with open(save_path, 'r') as sv_f:
-                with open(f'{cwd}\\save_clones\\{mod_info_data['save_identifier']}', 'w') as w_f:
+                with open(f'{cwd}\\save_clones\\{game_info_data['save_identifier']}', 'w') as w_f:
                     w_f.write(sv_f.read());
     
-    if(m_input[0].upper() == "QUIT"):
+    # :: BACK ::
+    if(m_input[0].upper() == 'BACK'):
+        in_settings = False;
+    
+    # :: SETTINGS ::
+    if(m_input[0].upper() == 'SETTINGS'):
+        in_settings = True;
+    
+    # :: COLOR ::
+    if(m_input[0].upper() == 'COLOR'):
+        OS.system(f'COLOR {m_input[1]}');
+        s_cmds['COLOR'][1] = m_input[1];
+        OS.system('CLS');
+    
+    # :: QUIT ::
+    if(m_input[0].upper() == 'QUIT'):
         quit();
     
